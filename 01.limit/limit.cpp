5c33dc1b27b3fa39910f7cccf78d1c8464c01d23
Feby <febysyarief.dev@gmail.com>
2026-01-21 21:28:33 +0700

    revising the script to be more modular

--- 01.limit/limit.cpp
+++ 01.limit/limit.cpp
@@ -2,19 +2,8 @@
 #include <cmath>
 #include <vector>
 
-//example: limit x mendekati 1 dari
-//f(x) = (x^3 - 1) / (x-1)
-
-double f(double x)
+double limit(double (*f)(double), double a, double e = 1e-12)
 {
-    // f(x) = (x^3 - 1) / (x-1)
-    return (std::pow(x, 3) - 1) / (x - 1);
-}
-
-int main()
-{
-    double a = 1.0;   // Titik limit
-    double e = 1e-12; // Toleransi kedekatan (epsilon)
     double limit_kiri, limit_kanan;
 
     std::vector<double> x_kiri, x_kanan;
@@ -51,11 +40,8 @@ int main()
         limit_kiri = fx;
     }
 
+    // Output Pendekatan dari Kanan
     fmt::print("\nPENDEKATAN DARI KANAN (x > {})\n", a);
-    fmt::print("{:<5} | {:<18} | {:<18}\n", "Step", "x", "f(x)");
-    fmt::print("{:-<48}\n", "");
-
-    fmt::print("PENDEKATAN DARI KANAN (x < {})\n", a);
     fmt::print("{:<5} | {:<18} | {:<18}\n", "Step", "x", "f(x)");
     fmt::print("{:-<48}\n", "");
 
@@ -82,12 +68,24 @@ int main()
         fmt::print("lim(x→{}) f(x) = {:.12f}\n", a, avg_limit);
         fmt::print("Nilai eksak    = 3.000000000000\n");
         fmt::print("Error          = {:.2e}\n", std::abs(avg_limit - 3.0));
+        return avg_limit;
     }
     else
     {
         fmt::print("\nLIMIT TIDAK ADA\n");
         fmt::print("  (Divergen atau Jump Discontinuity)\n");
+        return NAN;
     }
+}
 
+double f(double x)
+{
+    // f(x) = (x^3 - 1) / (x-1)
+    return (std::pow(x, 2) - 4) / (x - 2);
+}
+
+int main()
+{
+    limit(f, 2);
     return 0;
 }
